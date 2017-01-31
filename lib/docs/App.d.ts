97c8bb4ef7a07b0fd0860923e6f073dcca40cae7
devdoomari.circleci <devdoomari@gmail.com>
2017-01-31 09:39:47 +0000

    generated from:aa01082a5bf16928371eb541e344c7d026ea4a6f triggered by:devdoomari [ci skip]

---
+++ lib/docs/App.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+import * as React from 'react';
+import { History } from 'react-router';
+export declare type PropsType = {
+    history: History.History;
+};
+declare const App: React.SFC<PropsType>;
+export default App;
---
+++ lib_es6/docs/pages/StageDemoPage.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+import * as React from 'react';
+export declare type PropsType = {};
+export declare type StateType = {};
+export default class StageDemoPage extends React.Component<PropsType, StateType> {
+    render(): JSX.JSXElement;
+}
